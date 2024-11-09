658a275cbfa82cfe13d432e41773eae5bee58793
Zeniten <larskristian.marontelle@gmail.com>
2024-11-09 16:30:42 +0100

    Keep custom settings in separate file

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0" default))
+ '(nano-modeline-position 'nano-modeline-footer)
+ '(package-selected-packages
+   '(clojure-ts-mode corfu smartparens-mode orderless zenburn-theme marginalia nano-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -62,6 +62,9 @@
 ;; Revert Dired and other buffers
 (setq global-auto-revert-non-file-buffers t)
 
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
 ;(defvar space-map (make-sparse-keymap)
 ;  "Keymap for SPC commands.")
 
@@ -325,20 +328,3 @@
 (use-package clojure-ts-mode
   :hook ((clojure-ts-mode . eglot-ensure)
 	 (clojure-ts-mode . corfu-mode)))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0" default))
- '(nano-modeline-position 'nano-modeline-footer)
- '(package-selected-packages
-   '(clojure-ts-mode corfu smartparens-mode orderless zenburn-theme marginalia nano-theme)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
