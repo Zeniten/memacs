280ba6db860e90227a5b833e8b7636eb1c08adeb
Zeniten <larskristian.marontelle@gmail.com>
2024-11-13 23:15:54 +0100

    Stay classy for now

--- config/setup-clojure.el
+++ config/setup-clojure.el
@@ -6,23 +6,16 @@
   ;; load default config
   (require 'smartparens-config))
 
-(add-to-list 'auto-mode-alist '("\\.clj\\'" . clojure-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.cljs\\'" . clojure-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.cljc\\'" . clojure-ts-mode))
-
 ;; Clojure
 ;; TODO Just testing so far; no idea if this is correct
-(use-package clojure-ts-mode
-  :defer t
-  :hook ((clojure-ts-mode . eglot-ensure)
-	 (clojure-ts-mode . corfu-mode)))
+(use-package clojure-mode
+  :mode (("\\.clj\\'" . clojure-mode)
+	 ("\\.cljs\\'" . clojurescript-mode)
+	 ("\\.cljc\\'" . clojurec-mode)))
 
-(use-package eglot
-  :defer t
-  :hook (clojure-mode clojurescript-mode clojurec-mode)
-  :config
-  (add-to-list 'eglot-server-programs
-	       '(clojure-ts-mode . ("clojure-lsp"))))
+;(use-package eglot
+;  :defer t
+;  :hook (clojure-mode clojurescript-mode clojurec-mode))
 
 (use-package cider
   :defer t)
