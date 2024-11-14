716289e19b2816d1646d82ac9b0e14fd68f6a5a2
Zeniten <larskristian.marontelle@gmail.com>
2024-11-15 00:43:36 +0100

    Experiment with leader-key configuration

--- config/setup-clojure.el
+++ config/setup-clojure.el
@@ -6,9 +6,17 @@
   (require 'smartparens-config))
 
 (use-package clojure-mode
+  :after evil ; necessary?
   :mode (("\\.clj\\'" . clojure-mode)
 	 ("\\.cljs\\'" . clojurescript-mode)
 	 ("\\.cljc\\'" . clojurec-mode))
+  :config
+  ;; is this conventional? would general.el be better?
+  ;; what does spacemacs do?
+  (evil-set-leader '(normal visual) (kbd ",") t)
+  (evil-define-key '(normal visual) 'local
+    (kbd ",ef") 'cider-eval-defun-at-point
+    (kbd ",es") 'cider-eval-sexp-at-point)
   :hook ((clojure-mode . eglot-ensure)
 	 (clojurescript-mode . eglot-ensure)
 	 (clojurec-mode . eglot-ensure)))
