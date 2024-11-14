ee480cf064b7245f725c7c554d278570285e49a3
Zeniten <larskristian.marontelle@gmail.com>
2024-11-15 00:52:57 +0100

    Try general.el

--- config/setup-clojure.el
+++ config/setup-clojure.el
@@ -5,18 +5,29 @@
   ;; load default config
   (require 'smartparens-config))
 
+(use-package general
+  :after evil
+  :config
+  (general-create-definer clojure-leader-def
+			  :keymaps '(clojure-mode-map clojurescript-mode-map clojurec-mode-map)
+			  :prefix ","))
+
 (use-package clojure-mode
-  :after evil ; necessary?
+  ;:after evil ; necessary?
   :mode (("\\.clj\\'" . clojure-mode)
 	 ("\\.cljs\\'" . clojurescript-mode)
 	 ("\\.cljc\\'" . clojurec-mode))
   :config
   ;; is this conventional? would general.el be better?
   ;; what does spacemacs do?
-  (evil-set-leader '(normal visual) (kbd ",") t)
-  (evil-define-key '(normal visual) 'local
-    (kbd ",ef") 'cider-eval-defun-at-point
-    (kbd ",es") 'cider-eval-sexp-at-point)
+  ;(evil-set-leader '(normal visual) (kbd ",") t)
+  ;(evil-define-key '(normal visual) 'local
+  ;  (kbd ",ef") 'cider-eval-defun-at-point
+  ;  (kbd ",es") 'cider-eval-sexp-at-point)
+  (clojure-leader-def
+   :states '(normal visual)
+   "ef" 'cider-eval-defun-at-point
+   "es" 'cider-eval-sexp-at-point)
   :hook ((clojure-mode . eglot-ensure)
 	 (clojurescript-mode . eglot-ensure)
 	 (clojurec-mode . eglot-ensure)))
