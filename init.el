602dac20a37e0f1cb36e8c7084198f6949f11e36
Zeniten <larskristian.marontelle@gmail.com>
2024-11-28 22:03:42 +0100

    Remove mystery configuration

--- init.el
+++ init.el
@@ -145,21 +145,6 @@
   :config
   (global-corfu-mode 1))
 
-;; A few more useful configurations...
-(use-package emacs
-  :init
-  ;; TAB cycle if there are only few candidates
-  (setq completion-cycle-threshold 3)
-
-  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
-  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
-
-  ;; Enable indentation+completion using the TAB key.
-  ;; `completion-at-point' is often bound to M-TAB.
-  (setq tab-always-indent 'complete))
-
 (use-package recentf
   :custom
   (recentf-max-saved-items 50) ; show more recent files
