aaff738406a7092ddca5b938947a90070ae0ee21
Zeniten <larskristian.marontelle@gmail.com>
2024-12-01 22:32:59 +0100

    Use apparent best practice
    Perplexity:
    The sharp quote is equivalent to using the function special form and is
    preferred for several reasons:
    
    - It explicitly indicates that you are referring to a function object.
    - It allows the Lisp compiler to perform optimizations.
    - It helps catch errors if the referenced symbol is not actually a function.

--- core/core-memacs.el
+++ core/core-memacs.el
@@ -54,52 +54,52 @@
 
 ;; https://github.com/noctuid/evil-guide#keybindings-and-states
 (evil-define-key '(normal visual) 'global
-  (kbd "<SPC>fs") 'save-buffer
-  (kbd "<SPC>ff") 'find-file
-  (kbd "<SPC>fj") 'dired-jump
-  (kbd "<SPC>qq") 'kill-emacs
-  (kbd "<SPC>qr") 'restart-emacs
-  (kbd "<SPC>SPC") 'execute-extended-command ;meta
+  (kbd "<SPC>fs") #'save-buffer
+  (kbd "<SPC>ff") #'find-file
+  (kbd "<SPC>fj") #'dired-jump
+  (kbd "<SPC>qq") #'kill-emacs
+  (kbd "<SPC>qr") #'restart-emacs
+  (kbd "<SPC>SPC") #'execute-extended-command ;meta
 
-  (kbd "<SPC>pp") 'project-switch-project
-  (kbd "<SPC>pf") 'project-find-file
-  (kbd "<SPC>/") 'consult-ripgrep
+  (kbd "<SPC>pp") #'project-switch-project
+  (kbd "<SPC>pf") #'project-find-file
+  (kbd "<SPC>/") #'consult-ripgrep
 
-  (kbd "<SPC>ss") 'consult-line
+  (kbd "<SPC>ss") #'consult-line
 
-  (kbd "<SPC>gs") 'magit-status
+  (kbd "<SPC>gs") #'magit-status
 
-  (kbd "<SPC>hf") 'describe-function
-  (kbd "<SPC>hk") 'describe-key
-  (kbd "<SPC>hm") 'describe-mode
-  (kbd "<SPC>hv") 'describe-variable
+  (kbd "<SPC>hf") #'describe-function
+  (kbd "<SPC>hk") #'describe-key
+  (kbd "<SPC>hm") #'describe-mode
+  (kbd "<SPC>hv") #'describe-variable
 
-  (kbd "<SPC>bp") 'previous-buffer
-  (kbd "<SPC>bn") 'next-buffer
-  (kbd "<SPC>bb") 'consult-buffer
+  (kbd "<SPC>bp") #'previous-buffer
+  (kbd "<SPC>bn") #'next-buffer
+  (kbd "<SPC>bb") #'consult-buffer
 
-  (kbd "<SPC>wv") 'split-window-right
-  (kbd "<SPC>wV") 'split-window-right-and-focus
-  (kbd "<SPC>ws") 'split-window-below
-  (kbd "<SPC>wS") 'split-window-below-and-focus
-  (kbd "<SPC>wK") 'evil-window-move-very-top
-  (kbd "<SPC>wH") 'evil-window-move-far-left
-  (kbd "<SPC>wJ") 'evil-window-move-very-bottom
-  (kbd "<SPC>wL") 'evil-window-move-far-right
-  (kbd "<SPC>wd") 'delete-window
-  (kbd "<SPC>1") 'winum-select-window-1
-  (kbd "<SPC>2") 'winum-select-window-2
-  (kbd "<SPC>3") 'winum-select-window-3
-  (kbd "<SPC>4") 'winum-select-window-4
-  (kbd "<SPC>5") 'winum-select-window-5
-  (kbd "<SPC>6") 'winum-select-window-6
-  (kbd "<SPC>7") 'winum-select-window-7
-  (kbd "<SPC>8") 'winum-select-window-8
-  (kbd "<SPC>9") 'winum-select-window-9
+  (kbd "<SPC>wv") #'split-window-right
+  (kbd "<SPC>wV") #'split-window-right-and-focus
+  (kbd "<SPC>ws") #'split-window-below
+  (kbd "<SPC>wS") #'split-window-below-and-focus
+  (kbd "<SPC>wK") #'evil-window-move-very-top
+  (kbd "<SPC>wH") #'evil-window-move-far-left
+  (kbd "<SPC>wJ") #'evil-window-move-very-bottom
+  (kbd "<SPC>wL") #'evil-window-move-far-right
+  (kbd "<SPC>wd") #'delete-window
+  (kbd "<SPC>1") #'winum-select-window-1
+  (kbd "<SPC>2") #'winum-select-window-2
+  (kbd "<SPC>3") #'winum-select-window-3
+  (kbd "<SPC>4") #'winum-select-window-4
+  (kbd "<SPC>5") #'winum-select-window-5
+  (kbd "<SPC>6") #'winum-select-window-6
+  (kbd "<SPC>7") #'winum-select-window-7
+  (kbd "<SPC>8") #'winum-select-window-8
+  (kbd "<SPC>9") #'winum-select-window-9
   )
 
 (evil-define-key '(normal visual) prog-mode-map
-  (kbd "TAB") 'indent-for-tab-command
+  (kbd "TAB") #'indent-for-tab-command
   )
 
 
