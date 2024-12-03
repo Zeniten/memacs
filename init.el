3f29e6efcec8bf56891bbc6db9b565b8d9798ae3
Zeniten <larskristian.marontelle@gmail.com>
2024-12-03 23:08:27 +0100

    Update packages automatically

--- init.el
+++ init.el
@@ -90,6 +90,12 @@
         ("MELPA"        . 5)
         ("MELPA Stable" . 0)))
 
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
 (use-package catppuccin-theme
   ;:custom
   ;(catppuccin-flavor 'latte)
