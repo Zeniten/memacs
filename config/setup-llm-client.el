f0a194b88d15c70665f3bc841d5d8899c1c91a0f
Zeniten <larskristian.marontelle@gmail.com>
2024-12-05 23:30:46 +0100

    Add key binding

--- config/setup-llm-client.el
+++ config/setup-llm-client.el
@@ -7,7 +7,8 @@
   (evil-define-key '(normal visual) 'global
     (kbd "<SPC>la") #'gptel-add  ; add context
     (kbd "<SPC>lm") #'gptel-menu ; transient menu for preference, etc.
-    (kbd "<SPC>lr") #'gptel-rewrite)
+    (kbd "<SPC>lr") #'gptel-rewrite
+    (kbd "<SPC>ls") #'gptel-send)
   )
 
 (provide 'setup-llm-client)
