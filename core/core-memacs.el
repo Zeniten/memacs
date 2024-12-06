c3e1de3a011db0c24cd0efe26e44cb3cadc9015e
Zeniten <larskristian.marontelle@gmail.com>
2024-12-06 21:36:51 +0100

    Add focus versions to split-window functions

--- core/core-memacs.el
+++ core/core-memacs.el
@@ -84,6 +84,16 @@
   "<SPC> SPC" "M-x"
 )
 
+(defun memacs-split-window-right-and-focus ()
+  (interactive)
+  (split-window-right)
+  (other-window 1))
+
+(defun memacs-split-window-below-and-focus ()
+  (interactive)
+  (split-window-below)
+  (other-window 1))
+
 ;; https://github.com/noctuid/evil-guide#keybindings-and-states
 (evil-define-key '(normal visual) 'global
   (kbd "<SPC>fs") #'save-buffer
@@ -111,9 +121,9 @@
   (kbd "<SPC>bb") #'consult-buffer
 
   (kbd "<SPC>wv") #'split-window-right
-  (kbd "<SPC>wV") #'split-window-right-and-focus
+  (kbd "<SPC>wV") #'memacs-split-window-right-and-focus
   (kbd "<SPC>ws") #'split-window-below
-  (kbd "<SPC>wS") #'split-window-below-and-focus
+  (kbd "<SPC>wS") #'memacs-split-window-below-and-focus
   (kbd "<SPC>wK") #'evil-window-move-very-top
   (kbd "<SPC>wH") #'evil-window-move-far-left
   (kbd "<SPC>wJ") #'evil-window-move-very-bottom
