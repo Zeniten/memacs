af9c3baefa0c2ec1b90a3981f7a60800d88170e8
Zeniten <larskristian.marontelle@gmail.com>
2025-02-17 12:05:36 +0100

    Add changes to custom.el

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
    '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" "de8f2d8b64627535871495d6fe65b7d0070c4a1eb51550ce258cd240ff9394b0" default))
  '(nano-modeline-position 'nano-modeline-footer)
  '(package-selected-packages
-   '(esup gptel markdown-mode auto-dark eros org-roam general smartparens magit winum which-key evil-collection evil consult vertico cider treesit clojure-ts-mode corfu smartparens-mode orderless marginalia))
+   '(expand-region esup gptel markdown-mode auto-dark eros org-roam general smartparens magit winum which-key evil-collection evil consult vertico cider treesit clojure-ts-mode corfu smartparens-mode orderless marginalia))
  '(safe-local-variable-values '((cider-clojure-cli-aliases . "-A:dev"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
