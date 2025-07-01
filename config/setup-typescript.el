7eed665e4fe1e76d8396721a3f971febe9413c80
Zeniten <larskristian.marontelle@gmail.com>
2025-07-01 09:12:24 +0200

    Add Tree-sitter grammar for Dockerfile

--- config/setup-typescript.el
+++ config/setup-typescript.el
@@ -17,7 +17,7 @@
          ("\\.ts\\'"  . typescript-ts-mode)
          ("\\.jsx\\'" . tsx-ts-mode)
          ("\\.json\\'" .  json-ts-mode)
-         ("\\.Dockerfile\\'" . dockerfile-ts-mode)
+         ("\\Dockerfile\\'" . dockerfile-ts-mode)
          ;; More modes defined here...
          )
   :preface
@@ -31,10 +31,11 @@
                (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2" "src"))
                (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
                (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-               ;(elisp "https://github.com/Wilfred/tree-sitter-elisp")
+					;(elisp "https://github.com/Wilfred/tree-sitter-elisp")
                (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
                (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
-               (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))))
+               (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))
+	       (dockerfile . ("https://github.com/camdencheek/tree-sitter-dockerfile" "v0.2.0"))))
       (add-to-list 'treesit-language-source-alist grammar)
       ;; Only install `grammar' if we don't already have it
       ;; installed. However, if you want to *update* a grammar then
