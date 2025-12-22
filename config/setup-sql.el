5058fc27981eab9aed29b4b830cabc2febfd1355
Zeniten <larskristian.marontelle@gmail.com>
2025-12-22 16:49:32 +0100

    Standardize SQL configuration with deferred loading
    - Add :defer t for lazy loading
    - Add :commands for autoloading triggers
    - Add keybinding for sqlformat (, f)

--- config/setup-sql.el
+++ config/setup-sql.el
@@ -1,4 +1,10 @@
 ;; https://github.com/purcell/sqlformat
-(use-package sqlformat)
+(use-package sqlformat
+  :defer t
+  :commands (sqlformat sqlformat-buffer)
+  :init
+  (memacs/minor-leader-def
+    :keymaps 'sql-mode-map
+    "f" '(sqlformat :which-key "format SQL")))
 
 (provide 'setup-sql)
