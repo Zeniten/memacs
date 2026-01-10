4a3b6b46e666e658b2a24f9280d0000abe374037
Zeniten <larskristian.marontelle@gmail.com>
2026-01-10 18:41:39 +0100

    Add `json-navigator` for `restclient`
    Also:
    - increase response-size threshold, as default was kind of low
    - use `json-ts-mode` to view JSON

--- config/setup-rest-client.el
+++ config/setup-rest-client.el
@@ -1,5 +1,18 @@
+(use-package json-navigator
+  :defer t)
+
 (use-package restclient
   :defer t
-  :mode (("\\.http\\'" . restclient-mode)))
+  :mode (("\\.http\\'" . restclient-mode))
+  :custom
+  (restclient-response-size-threshold 500000)
+  :bind (:map restclient-response-buffer-map
+         ;; Override 'q' to always close the response window properly.
+         ;; Without this, after using json-navigator, 'q' would switch to the
+         ;; source buffer instead of closing the response window.
+         ("q" . delete-window))
+  :config
+  (setf (alist-get "application/json" restclient-content-type-modes nil nil #'equal)
+        'json-ts-mode))
 
 (provide 'setup-rest-client)
