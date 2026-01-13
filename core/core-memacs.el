dbbc3e9dac8123312f1e8eb15255803c2d3df857
Zeniten <larskristian.marontelle@gmail.com>
2026-01-13 13:21:00 +0100

    Run `whitespace-cleanup`

--- core/core-memacs.el
+++ core/core-memacs.el
@@ -38,36 +38,36 @@
 (use-package treesit
   :ensure nil
   :mode (("\\.tsx\\'" . tsx-ts-mode)
-         ("\\.js\\'"  . typescript-ts-mode)
-         ("\\.mjs\\'" . typescript-ts-mode)
-         ("\\.mts\\'" . typescript-ts-mode)
-         ("\\.cjs\\'" . typescript-ts-mode)
-         ("\\.ts\\'"  . typescript-ts-mode)
-         ("\\.jsx\\'" . tsx-ts-mode)
-         ("\\.json\\'" .  json-ts-mode)
-         ("\\Dockerfile\\'" . dockerfile-ts-mode))
+	 ("\\.js\\'"  . typescript-ts-mode)
+	 ("\\.mjs\\'" . typescript-ts-mode)
+	 ("\\.mts\\'" . typescript-ts-mode)
+	 ("\\.cjs\\'" . typescript-ts-mode)
+	 ("\\.ts\\'"  . typescript-ts-mode)
+	 ("\\.jsx\\'" . tsx-ts-mode)
+	 ("\\.json\\'" .  json-ts-mode)
+	 ("\\Dockerfile\\'" . dockerfile-ts-mode))
   :preface
   (defun memacs/setup-install-grammars ()
     "Install Tree-sitter grammars if they are absent."
     (interactive)
     (dolist (grammar
-             '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
-               (bash "https://github.com/tree-sitter/tree-sitter-bash")
-               (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
-               (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2" "src"))
-               (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
-               (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-               ;; (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-               (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
-               (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
-               (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))
-               (dockerfile . ("https://github.com/camdencheek/tree-sitter-dockerfile" "v0.2.0"))))
+	     '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+	       (bash "https://github.com/tree-sitter/tree-sitter-bash")
+	       (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+	       (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2" "src"))
+	       (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+	       (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+	       ;; (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+	       (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
+	       (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+	       (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))
+	       (dockerfile . ("https://github.com/camdencheek/tree-sitter-dockerfile" "v0.2.0"))))
       (add-to-list 'treesit-language-source-alist grammar)
       ;; Only install `grammar' if we don't already have it
       ;; installed. However, if you want to *update* a grammar then
       ;; this obviously prevents that from happening.
       (unless (treesit-language-available-p (car grammar))
-        (treesit-install-language-grammar (car grammar)))))
+	(treesit-install-language-grammar (car grammar)))))
 
   ;; Optional, but recommended. Tree-sitter enabled major modes are
   ;; distinct from their ordinary counterparts.
@@ -76,15 +76,15 @@
   ;; that this does *not* extend to hooks! Make sure you migrate them
   ;; also
   (dolist (mapping
-           '((css-mode . css-ts-mode)
-             (typescript-mode . typescript-ts-mode)
-             (js-mode . typescript-ts-mode)
-             (js2-mode . typescript-ts-mode)
-             (bash-mode . bash-ts-mode)
-             (json-mode . json-ts-mode)
-             (js-json-mode . json-ts-mode)
-             (sh-mode . bash-ts-mode)
-             (sh-base-mode . bash-ts-mode)))
+	   '((css-mode . css-ts-mode)
+	     (typescript-mode . typescript-ts-mode)
+	     (js-mode . typescript-ts-mode)
+	     (js2-mode . typescript-ts-mode)
+	     (bash-mode . bash-ts-mode)
+	     (json-mode . json-ts-mode)
+	     (js-json-mode . json-ts-mode)
+	     (sh-mode . bash-ts-mode)
+	     (sh-base-mode . bash-ts-mode)))
     (add-to-list 'major-mode-remap-alist mapping))
   :config
   (memacs/setup-install-grammars))
@@ -149,7 +149,7 @@
   :ensure nil
   :init
   (setq which-key-idle-delay 0.4
-        which-key-idle-secondary-delay 0.05)
+	which-key-idle-secondary-delay 0.05)
   :config
   (which-key-mode 1))
 
@@ -162,8 +162,8 @@
   (dired-sidebar-subtree-line-prefix "•")
   :commands (dired-sidebar-toggle-sidebar)
   :hook (dired-sidebar-mode . (lambda ()
-              (unless (file-remote-p default-directory)
-                (auto-revert-mode))))
+	      (unless (file-remote-p default-directory)
+		(auto-revert-mode))))
   :config
   (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
   (push 'rotate-windows dired-sidebar-toggle-hidden-commands))
@@ -189,10 +189,10 @@
   :config
   ;; Auto-focus the eldoc buffer when displayed
   (advice-add 'eldoc-doc-buffer :after
-              (lambda (&rest _)
-                (when-let ((buf (get-buffer "*eldoc*")))
-                  (when (get-buffer-window buf)
-                    (select-window (get-buffer-window buf)))))))
+	      (lambda (&rest _)
+		(when-let ((buf (get-buffer "*eldoc*")))
+		  (when (get-buffer-window buf)
+		    (select-window (get-buffer-window buf)))))))
 
 (use-package esup
   :defer t)
